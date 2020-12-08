ded53a777e9b6f75d7dc2996964443109c729a32
iori <matsuhar@gmail.com>
2020-12-08 11:17:27 +0100

    try fetching project info

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const payload = github.context.payload;
+
+  var project = {
+    owner: payload.owner.name,
+    repo: payload.repository.name,
+    packageName: core.getInput('packageName'),
+    commitId: payload.head_commit.id
+  };
+
+  console.log(`project: ${JSON.stringify(project)}`);
 } catch (error) {
   core.setFailed(error.message);
 }
