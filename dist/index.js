c2b79979645a718d6bb3bb75ebfeccdd8396cd54
iori <matsuhar@gmail.com>
2020-12-09 09:58:41 +0100

    v0.35

--- dist/index.js
+++ dist/index.js
@@ -64,7 +64,7 @@ async function run() {
     const hieDirectory = core.getInput('hieDirectory');
     console.log("5");
     await exec.exec('pwd', []);
-    await exec.exec('ls', ["-a", izunaBuilderExeFullPath]);
+    await exec.exec('ls', ["-l", izunaBuilderExeFullPath]);
     await exec.exec(izunaBuilderExeFullPath,
                     [ '--hie-directory=' + hieDirectory,
                       '--user=' + project.user,
