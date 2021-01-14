20e23afe0c5423b5449fcbab715435dd4c76fbd6
iori <matsuhar@gmail.com>
2021-01-14 16:06:42 +0100

    v0.56

--- dist/index.js
+++ dist/index.js
@@ -53,7 +53,7 @@ async function run() {
                            ]
                    );
 
-    const izunaBuilderUrl = "https://izuna-builder.patchgirl.io/api/" + project.ghcVersion.replace(/\./g, "") + "/projectInfo/" + project.user + "/" + project.repo + "/" + project.packageName + "/" + project.commitId;
+    const izunaBuilderUrl = "https://izuna-builder.izuna.app/api/" + project.ghcVersion.replace(/\./g, "") + "/projectInfo/" + project.user + "/" + project.repo + "/" + project.packageName + "/" + project.commitId;
     exec.exec('curl', [ "--form",
                         '--file=@' + tarName,
                         izunaBuilderUrl
